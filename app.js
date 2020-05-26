235bc67f3f4cab9c5e312f951044563dbe52fb8a
hieu-ng <hieutnguyen06@gmail.com>
2020-05-27 09:59:10 +1000

    Display search results to page

--- app.js
+++ app.js
@@ -14,9 +14,25 @@ function searchMeal(e) {
 	if (term.trim()) {
 		fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
 			.then(res => res.json())
-			.then(data {
-				resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`
-			})
+			.then(data => {
+				resultHeading.innerHTML = `<h2>Search results for '${term}'</h2>`;
+
+				if (data.meals === null) {
+					resultHeading.innerHTML =
+						`<p>There is no result for '${term}'. Try again.</p>`;
+				} else {
+					mealsEl.innerHTML = data.meals.map(meal =>
+							`<div class="meal">
+							<img src="${meal.strMealThumb}" alt="${meal.strMeal}"
+							<div class="mean-info" data-mealID="${mealidMeal}">
+								<h3>${meal.strMeal}</h3>
+							</div>
+						</div>`
+						)
+						.join('')
+				}
+			});
+		search.value = '';
 	} else {
 		alert('Please enter a search term')
 	}
